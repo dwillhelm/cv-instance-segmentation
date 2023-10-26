b60b0ef92d8c10f747dec75330f02bc2d316c758
dwillhelm <dwillhelm@enthought.com>
2023-10-26 16:06:53 -0500

    add prerun

---
+++ prerun.py
@@ -0,0 +1,23 @@
+import os
+import torch
+
+
+
+def prerun(): 
+    print(f'\nPytorch version: {torch.__version__}')
+    os.system('nvcc --version')
+    # setting device on GPU if available, else CPU
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+
+    #Additional Info when using cuda
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+    print()
+
+if __name__ == '__main__':
+    prerun()
