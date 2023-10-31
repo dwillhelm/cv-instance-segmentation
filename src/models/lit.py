3687fa835b5bdca2c63f6d6a4e7695121e148ada
dwillhelm <dwillhelm@enthought.com>
2023-10-31 16:44:17 -0500

    WIP: pytorch lightning training

--- src/models/lit.py
+++ src/models/lit.py
@@ -2,8 +2,12 @@ from typing import Any
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.types import STEP_OUTPUT, OptimizerLRScheduler
 import torch
+import torchvision
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor, MaskRCNN
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
-from src.models import format_MaskRCNN
+from src.models.models import format_MaskRCNN
 
 class VGCFModel(pl.LightningModule):
 
@@ -11,12 +15,38 @@ class VGCFModel(pl.LightningModule):
         super().__init__(*args, **kwargs)
         self.save_hyperparameters()
 
-        if fiber_type is "lenght": 
+        if fiber_type == "lenght": 
             ... 
-        elif fiber_type is "width": 
+        elif fiber_type == "width": 
             ... 
         
-        self.model = format_MaskRCNN(num_classes=2)
+        model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
+        num_classes = 2 # background and object (fibers, a person, etc.)
+
+        # get number of input features for the classifier
+        in_features = model.roi_heads.box_predictor.cls_score.in_features
+        # replace the pre-trained head with a new one
+        model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+        # now get the number of input features for the mask classifier
+        in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+        hidden_layer = 256
+        # and replace the mask predictor with a new one
+        model.roi_heads.mask_predictor = MaskRCNNPredictor(
+            in_features_mask,
+            hidden_layer,
+            num_classes,
+        )
+        self.model = model
+    
+    def forward(self, images, targets) -> Any:
+        return self.model(images, targets)
+        
+    def shared_step(self, batch): 
+        images, targets = batch
+        out:dict = self(images, targets)
+        loss = sum(_loss for _loss in out.values())
+        return loss
     
     def training_step(self, batch, batch_idx) -> STEP_OUTPUT:
         loss = self.shared_step(batch)
@@ -25,16 +55,10 @@ class VGCFModel(pl.LightningModule):
     def validation_step(self, batch, batch_idx) -> STEP_OUTPUT:
         loss = self.shared_step(batch)
         return loss
-
-    def shared_step(self, batch): 
-        images, targets = batch
-        out:dict = self(images, targets)
-        loss = sum(_loss for _loss in out.values())
-        return loss
         
     def configure_optimizers(self) -> OptimizerLRScheduler:
         params = [p for p in self.model.parameters() if p.requires_grad]
         optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
         return optimizer
-        
+
 
